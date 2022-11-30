d6c9c2a1248956a50bae2a77a2e14944c1b4c274
Mingxuan Qin <qmx0921@gmail.com>
2022-11-30 19:20:53 +0100

    adjust homepage moon location

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
-let coverLogo = document.querySelector('.cover > .cover-image-wrapper');
+let coverLogo = document.querySelector('.cover > .cover-image-wrapper')
+let coverArea = document.querySelector('.cover')
+
+let originalLogoPosition = (coverArea.offsetHeight - coverLogo.offsetHeight) * 0.48
+
+coverLogo.style.bottom = String(originalLogoPosition) + 'px';
 
 document.addEventListener('scroll', () => {
-    coverLogo.style.bottom = String(-document.documentElement.scrollTop + 0.18 * getViewport()[1]) + 'px';
+    coverLogo.style.bottom = String(-document.documentElement.scrollTop + originalLogoPosition) + 'px';
 });
